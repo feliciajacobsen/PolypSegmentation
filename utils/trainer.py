5559f6b7493574138f7a7588f3da94a555b3ca8c
Felicia Ly Jacobsen <feliciaj@srl-login1.cm.cluster>
2021-09-20 14:25:09 +0200

    add stuff

--- utils/trainer.py
+++ utils/trainer.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 from model import UNET
 
 
-def train_model(loader, model, device, optimizer, loss_function):
+def train_model(loader, model, device, optimizer, criterion):
     """
     Function perform one epoch on entire dataset and outputs loss for each batch.
 
@@ -20,56 +20,43 @@ def train_model(loader, model, device, optimizer, loss_function):
 
         optimizer (torch object): optimization algorithm. 
 
-        loss_function (torch oject): loss function with backward method.
+        criterion (torch oject): loss function with backward method.
 
     Returns:
         float: mean loss over batches
     """
 
-    tqdm_loader = tqdm(loader)
+    tqdm_loader = tqdm(loader) # make progress bar
+    scaler = torch.cuda.amp.GradScaler() # gradient scaling to prevent undeflow
 
     model.train()
     losses = []
 
     for batch_idx, (data, targets) in enumerate(tqdm_loader):
-        data = data.to(device=device) # move data to same device as gradients are computed
+        # move data and masks to same device as computed gradients
+        data = data.to(device=device) 
         targets = targets.float().to(device=device)
 
+        # mixed precision training
         with torch.cuda.amp.autocast():
-            output = model(data)
-            loss = loss_function(output, targets)
+            output = model(data) 
+            loss = criterion(output, targets)
 
+        # backprop
         optimizer.zero_grad()
-        output = model(data)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
 
-        loss_function = criterion(data, targets)
-        loss_function.backward()
-        optimizer.step()
+        # update tqdm loop
+        tqdm_loader.set_postfix(loss=loss.item())
+        
+        # update loss
         losses.append(loss.item())
 
 
 
 
-def main():
-    pass
-
-
-
-
-def run_model(root_dir, use_GPU=True):
-    config = dict()
-    config["device"] = "cuda" if torch.cuda.is_available() else "cpu"
-    config["lr"] = 1e-4
-    config["batch_size"] = 64
-    config["num_epochs"] = 3
-    config["num_workers"] = 2
-    config["num_classes"] = 1
-    #config["image_height"] = 160 # 1280 originally
-    #config["image_width"] = 240 # 1918 originally
-    config["pin_memory"] = True
-    config["load_model"] = False
-    config["img_dir"] = "data/Kvasir-SEG/images/"
-    config["mask_dir"] = "data/Kvasir-SEG/masks/"
 
 if __name__ == "__main__":
-    run_model()
+    train_model
