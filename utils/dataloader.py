2e0e2e180b1599374d4ca4acd4ff81494da5834b
Felicia Ly Jacobsen <feliciaj@srl-login1.cm.cluster>
2021-09-20 10:55:13 +0200

    add train function and loader

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -15,7 +15,7 @@ def get_mean_std(loader):
 
     channels_sum, channels_squared_sum, num_batches = 0, 0, 0
     
-    for data,_ in loader:
+    for data, _ in loader:
         channels_sum += torch.mean(data, dim=[0,2,3]) # don't sum across channel dim
         channels_squared_sum += torch.mean(data**2, dim=[0,2,3])
         num_batches += 1
@@ -24,10 +24,6 @@ def get_mean_std(loader):
     std = (channels_squared_sum/num_batches-mean**2)**0.5
 
     return mean, std
-"""
-_, train_loader = FashionMNIST_dataset(train=True)
-mean, std = get_mean_std(train_loader) 
-"""
 
 
 class PolypDataset(Dataset):
@@ -47,8 +43,8 @@ class PolypDataset(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.image_dir, self.images[index])
         mask_path = os.path.join(self.mask_dir, self.images[index])
-        image = np.array(Image.open(img_path).convert("RGB"))
-        mask = np.array(Image.open(mask_path).convert("L"), dtype = np.float32) # greyskale = L for PIL
+        image = np.array(Image.open(img_path, "r").convert("RGB"))
+        mask = np.array(Image.open(mask_path, "r").convert("L"), dtype = np.float32) # greyskale = L for PIL
         mask[mask==255.0] = 1.0 # 255 decimal code for white, change this to 1 due to sigmoid on output.
 
         if self.transform is not None:
---
+++ utils/trainer.py
@@ -0,0 +1,36 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+
+
+def train_model(loader, model, optimizer, loss_function, scaler):
+    pass
+
+
+def main():
+    pass
+
+
+
+
+def run_model(root_dir, use_GPU=True):
+    config = dict()
+    config["device"] = "cuda" if torch.cuda.is_available() else "cpu"
+    config["lr"] = 1e-4
+    config["batch_size"] = 64
+    config["num_epochs"] = 3
+    config["num_workers"] = 2
+    config["num_classes"] = 1
+    #config["image_height"] = 160 # 1280 originally
+    #config["image_width"] = 240 # 1918 originally
+    config["pin_memory"] = True
+    config["load_model"] = False
+    config["img_dir"] = "data/Kvasir-SEG/images/"
+    config["mask_dir"] = "data/Kvasir-SEG/masks/"
+
+if __name__ == "__main__":
+    run_model()
