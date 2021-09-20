1a020c1bcf2681fc74e0635748482b56a52ac17b
Felicia Ly Jacobsen <feliciaj@srl-login1.cm.cluster>
2021-09-20 11:25:24 +0200

    add trainer

--- utils/trainer.py
+++ utils/trainer.py
@@ -7,8 +7,47 @@ import torch.optim as optim
 from model import UNET
 
 
-def train_model(loader, model, optimizer, loss_function, scaler):
-    pass
+def train_model(loader, model, device, optimizer, loss_function):
+    """
+    Function perform one epoch on entire dataset and outputs loss for each batch.
+
+    Args:
+        loader (object): iterable-style dataset.
+
+        model (class): provides with a forward method.
+
+        device (cuda object): cpu or gpu.
+
+        optimizer (torch object): optimization algorithm. 
+
+        loss_function (torch oject): loss function with backward method.
+
+    Returns:
+        float: mean loss over batches
+    """
+
+    tqdm_loader = tqdm(loader)
+
+    model.train()
+    losses = []
+
+    for batch_idx, (data, targets) in enumerate(tqdm_loader):
+        data = data.to(device=device) # move data to same device as gradients are computed
+        targets = targets.float().to(device=device)
+
+        with torch.cuda.amp.autocast():
+            output = model(data)
+            loss = loss_function(output, targets)
+
+        optimizer.zero_grad()
+        output = model(data)
+
+        loss_function = criterion(data, targets)
+        loss_function.backward()
+        optimizer.step()
+        losses.append(loss.item())
+
+
 
 
 def main():
